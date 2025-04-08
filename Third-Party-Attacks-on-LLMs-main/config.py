0484a5e11885a085b4062bd5e98bc9c1ed93412d
Saeedfayyaz <fayyazsaeed725@GMAIL.COM>
2025-04-09 02:15:59 +1000

    Update config.py

--- Third-Party-Attacks-on-LLMs-main/config.py
+++ Third-Party-Attacks-on-LLMs-main/config.py
@@ -1,5 +1,5 @@
 # settings
-OPENAI_API_KEY = "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" # Insert Key Here
+OPENAI_API_KEY = "" # Insert Key Here
 API_KEY = "dfa71d624d70486cb70131629230908" # Insert Key Here
 GEMINI_API_KEY = "" # Insert Key Here
 API_HOST = "api-football-v1.p.rapidapi.com"
